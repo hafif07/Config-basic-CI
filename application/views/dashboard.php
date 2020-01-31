ac96a3affaf1fd1a4fc82826702c83986593f4cd
hafif07 <mhafif29@gmail.com>
2020-01-31 20:08:14 +0700

    menambahkan template footer

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -276,30 +276,3 @@
     <!-- Control sidebar content goes here -->
   </aside>
   <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.2
-    </div>
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="<?php echo base_url()?>assets/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap -->
-<script src="<?php echo base_url()?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE -->
-<script src="<?php echo base_url()?>assets/dist/js/adminlte.js"></script>
-
-<!-- OPTIONAL SCRIPTS -->
-<script src="<?php echo base_url()?>assets/plugins/chart.js/Chart.min.js"></script>
-<script src="<?php echo base_url()?>assets/dist/js/demo.js"></script>
-<script src="<?php echo base_url()?>assets/dist/js/pages/dashboard3.js"></script>
-</body>
-</html>
